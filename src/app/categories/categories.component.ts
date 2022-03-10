dd05f95ae93210cc6d81e9b95a1bdc6447891ab2
Daniel Schmitz <danieljfa@mail.com>
2022-03-10 10:31:10 -0300

    List insted table over small screens

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTable, MatTableDataSource } from '@angular/material/table';
+import { lastValueFrom } from 'rxjs';
 import { Category } from './category.dto';
 import { CategoryService } from './category.service';
 
@@ -24,7 +25,7 @@ export class CategoriesComponent implements OnInit {
   dataSource!: MatTableDataSource<Category>;
 
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
-  displayedColumns = ['id', 'name', 'description','actions'];
+  displayedColumns = ['id', 'name', 'description', 'actions'];
 
   showForm: Boolean = false;
 
@@ -40,47 +41,72 @@ export class CategoriesComponent implements OnInit {
     this.refreshData();
   }
 
-  refreshData() {
+  // refreshData() {
+  //   this.showLoading = true;
+
+  //   this.categoryService.getAll().subscribe((categories) => {
+  //     this.dataSource = new MatTableDataSource(categories);
+  //     // this.table.dataSource = this.dataSource;
+  //     this.dataSource.sort = this.sort;
+  //     this.dataSource.paginator = this.paginator;
+
+  //     // just a test to see loading bar
+  //     setTimeout(() => {
+  //       this.showLoading = false;
+  //     }, 2000);
+
+  //   });
+
+  // }
+
+  async refreshData() {
     this.showLoading = true;
 
-    this.categoryService.getAll().subscribe((categories) => {
+    try {
+
+      const categories: Category[] = await lastValueFrom(
+        this.categoryService.getAll()
+      );
       this.dataSource = new MatTableDataSource(categories);
-      // this.table.dataSource = this.dataSource;
       this.dataSource.sort = this.sort;
       this.dataSource.paginator = this.paginator;
 
-      // just a test to see loading bar
+
+    } catch (error) {
+
+      console.log("opsss!!", error);
+
+    } finally {
       setTimeout(() => {
         this.showLoading = false;
       }, 2000);
-
-
-    });
+    }
 
   }
 
   onNewCategoryClick() {
     this.category = {
-      id: 0, name: '', description: ''
-    }
+      id: 0,
+      name: '',
+      description: '',
+    };
     this.showForm = true;
   }
 
-  onEditCategoryClick(category:Category) {
-    console.log("edit category", category)
+  onEditCategoryClick(category: Category) {
+    console.log('edit category', category);
     this.category = category;
     this.showForm = true;
   }
 
-  onDeleteCategoryClick(category:Category) {
-    console.log("delete category", category)
+  onDeleteCategoryClick(category: Category) {
+    console.log('delete category', category);
 
     if (confirm(`Delete "${category.name}" with id ${category.id} ?`)) {
-      this.categoryService.delete(category.id).subscribe(
-        () => this.refreshData()
-      )
+      this.categoryService
+        .delete(category.id)
+        .subscribe(() => this.refreshData());
     }
-
   }
 
   onBackForm() {
@@ -100,7 +126,6 @@ export class CategoriesComponent implements OnInit {
         this.refreshData();
         this.showLoadingForm = false;
       }, 2000);
-
     });
   }
 }
