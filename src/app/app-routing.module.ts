0c1083bad0c5bd538dc6dd13a1281e1d3101f394
Daniel Schmitz <danieljfa@mail.com>
2022-03-11 13:29:17 -0300

    Include supplier list as default sub route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,22 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CategoriesComponent } from './categories/categories.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { SuppliersListComponent } from './suppliers/suppliers-list/suppliers-list.component';
 import { SuppliersComponent } from './suppliers/suppliers.component';
 
 const routes: Routes = [
-  {path:'categories', component:CategoriesComponent},
-  {path:'suppliers', component:SuppliersComponent},
-  {path:'', component:DashboardComponent}
+  { path: 'categories', component: CategoriesComponent },
+  {
+    path: 'suppliers',
+    component: SuppliersComponent,
+    children: [
+      {
+        path: '',
+        component: SuppliersListComponent
+      }
+    ]
+  },
+  { path: '', component: DashboardComponent }
 ]
 
 
