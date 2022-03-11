b572f8d46d78405f4dd737b71241baa1b5a15ea1
Daniel Schmitz <danieljfa@mail.com>
2022-03-11 13:19:53 -0300

    Suppliers route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CategoriesComponent } from './categories/categories.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { SuppliersComponent } from './suppliers/suppliers.component';
 
 const routes: Routes = [
   {path:'categories', component:CategoriesComponent},
+  {path:'suppliers', component:SuppliersComponent},
   {path:'', component:DashboardComponent}
 ]
 
