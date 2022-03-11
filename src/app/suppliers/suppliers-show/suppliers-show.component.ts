2f336a2f8ed3c77ef0ec375a6beb87723e4600d5
Daniel Schmitz <danieljfa@mail.com>
2022-03-11 18:59:04 -0300

    getting id router param from url

--- src/app/suppliers/suppliers-show/suppliers-show.component.ts
+++ src/app/suppliers/suppliers-show/suppliers-show.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-suppliers-show',
@@ -8,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SuppliersShowComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route:ActivatedRoute) { }
+
+  id:string='';
 
   ngOnInit(): void {
+    this.id = this.route.snapshot.paramMap.get('id') || '';
+    
   }
 
 }
