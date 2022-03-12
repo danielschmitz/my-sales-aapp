77a5329948602d26520f109d14a77090a90e044e
Daniel Schmitz <danieljfa@mail.com>
2022-03-12 09:21:12 -0300

    routind edit form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CategoriesComponent } from './categories/categories.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { SuppliersEditComponent } from './suppliers/suppliers-edit/suppliers-edit.component';
 import { SuppliersListComponent } from './suppliers/suppliers-list/suppliers-list.component';
 import { SuppliersShowComponent } from './suppliers/suppliers-show/suppliers-show.component';
 import { SuppliersComponent } from './suppliers/suppliers.component';
@@ -19,6 +20,10 @@ const routes: Routes = [
       {
         path: 'show/:id',
         component: SuppliersShowComponent
+      },
+      {
+        path: 'edit/:id',
+        component: SuppliersEditComponent
       }
     ]
   },
