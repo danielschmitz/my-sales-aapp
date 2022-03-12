c9b7462b32aff453e604eb5866bb28f132c3c593
Daniel Schmitz <danieljfa@mail.com>
2022-03-12 08:59:23 -0300

    create form and 1st field Compnay Name

--- src/app/suppliers/suppliers-edit/suppliers-edit.component.ts
+++ src/app/suppliers/suppliers-edit/suppliers-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { lastValueFrom } from 'rxjs';
 import { Supplier } from '../supplier.dto';
@@ -14,10 +15,12 @@ export class SuppliersEditComponent implements OnInit {
 
   id!:Number;
   supplier!: Supplier;
+  supplierForm!: FormGroup;
 
   constructor(
     private route:ActivatedRoute,
-    private supplierService:SupplierService
+    private supplierService:SupplierService,
+    private fb: FormBuilder
     ) { }
 
   async ngOnInit() {
@@ -25,7 +28,33 @@ export class SuppliersEditComponent implements OnInit {
 
     if (this.id) {
       this.supplier = await lastValueFrom(this.supplierService.getById(this.id))
+    
+      this.supplierForm = this.fb.group({
+        id: [this.supplier.id],
+        companyName: [this.supplier.companyName, [Validators.required, Validators.minLength(3)]],
+        contactName: [this.supplier.contactName, , [Validators.required, Validators.minLength(3)]],
+        contactTitle: [this.supplier.contactTitle],
+        address: this.fb.group(
+          {
+            city: [this.supplier.address.city],
+            country: [this.supplier.address.country],
+            phone: [this.supplier.address.phone],
+            postalCode: [this.supplier.address.postalCode],
+            region: [this.supplier.address.region],
+            street: [this.supplier.address.street]
+          }
+        )
+      })
+    
+    
+    
+    
     }
+    
+  }
+
+  onSubmit() {
+    console.log('submit')
   }
 
 }
