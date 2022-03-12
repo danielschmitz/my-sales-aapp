f052ad36c3b31e715ca77e9a4c0ae11f0506f5b6
Daniel Schmitz <danieljfa@mail.com>
2022-03-12 09:26:59 -0300

    load form data Async (++import)

--- src/app/suppliers/suppliers-edit/suppliers-edit.component.ts
+++ src/app/suppliers/suppliers-edit/suppliers-edit.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
-import { lastValueFrom } from 'rxjs';
+import { lastValueFrom, Observable } from 'rxjs';
 import { Supplier } from '../supplier.dto';
 import { SupplierService } from '../supplier.service';
 
@@ -13,44 +13,42 @@ import { SupplierService } from '../supplier.service';
 })
 export class SuppliersEditComponent implements OnInit {
 
-  id!:Number;
-  supplier!: Supplier;
+  id!: Number;
+  supplier!: Observable<Supplier>;
   supplierForm!: FormGroup;
 
   constructor(
-    private route:ActivatedRoute,
-    private supplierService:SupplierService,
+    private route: ActivatedRoute,
+    private supplierService: SupplierService,
     private fb: FormBuilder
-    ) { }
+  ) { }
 
   async ngOnInit() {
-    this.id = +(this.route.snapshot.paramMap.get('id')||0);
+
+    this.supplierForm = this.fb.group({
+      id: [''],
+      companyName: ['', [Validators.required, Validators.minLength(3)]],
+      contactName: ['',[Validators.required, Validators.minLength(3)]],
+      contactTitle: [''],
+      address: this.fb.group(
+        {
+          city: [''],
+          country: [''],
+          phone: [''],
+          postalCode: [''],
+          region: [''],
+          street: ['']
+        }
+      )
+    })
+
+    this.id = +(this.route.snapshot.paramMap.get('id') || 0);
 
     if (this.id) {
-      this.supplier = await lastValueFrom(this.supplierService.getById(this.id))
-    
-      this.supplierForm = this.fb.group({
-        id: [this.supplier.id],
-        companyName: [this.supplier.companyName, [Validators.required, Validators.minLength(3)]],
-        contactName: [this.supplier.contactName, , [Validators.required, Validators.minLength(3)]],
-        contactTitle: [this.supplier.contactTitle],
-        address: this.fb.group(
-          {
-            city: [this.supplier.address.city],
-            country: [this.supplier.address.country],
-            phone: [this.supplier.address.phone],
-            postalCode: [this.supplier.address.postalCode],
-            region: [this.supplier.address.region],
-            street: [this.supplier.address.street]
-          }
-        )
-      })
-    
-    
-    
-    
+      this.supplier = this.supplierService.getById(this.id)
+      this.supplierForm.patchValue(await lastValueFrom(this.supplier))
     }
-    
+
   }
 
   onSubmit() {
