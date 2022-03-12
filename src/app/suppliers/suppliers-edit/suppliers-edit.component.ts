f6d589c15c457bee941f38aac350cd7f0fc99139
Daniel Schmitz <danieljfa@mail.com>
2022-03-12 16:22:48 -0300

    supplier form

--- src/app/suppliers/suppliers-edit/suppliers-edit.component.ts
+++ src/app/suppliers/suppliers-edit/suppliers-edit.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { lastValueFrom, Observable } from 'rxjs';
 import { Supplier } from '../supplier.dto';
@@ -14,39 +13,20 @@ import { SupplierService } from '../supplier.service';
 export class SuppliersEditComponent implements OnInit {
 
   id!: Number;
-  supplier!: Observable<Supplier>;
-  supplierForm!: FormGroup;
+  supplierObservable!: Observable<Supplier>;
+  supplier!: Supplier;
 
   constructor(
     private route: ActivatedRoute,
     private supplierService: SupplierService,
-    private fb: FormBuilder
   ) { }
 
   async ngOnInit() {
-
-    this.supplierForm = this.fb.group({
-      id: [''],
-      companyName: ['', [Validators.required, Validators.minLength(3)]],
-      contactName: ['',[Validators.required, Validators.minLength(3)]],
-      contactTitle: [''],
-      address: this.fb.group(
-        {
-          city: [''],
-          country: [''],
-          phone: [''],
-          postalCode: [''],
-          region: [''],
-          street: ['']
-        }
-      )
-    })
-
     this.id = +(this.route.snapshot.paramMap.get('id') || 0);
 
     if (this.id) {
-      this.supplier = this.supplierService.getById(this.id)
-      this.supplierForm.patchValue(await lastValueFrom(this.supplier))
+      this.supplierObservable = this.supplierService.getById(this.id)
+      this.supplier = await lastValueFrom(this.supplierObservable)
     }
 
   }
--- src/app/suppliers/suppliers-form/suppliers-form.component.ts
+++ src/app/suppliers/suppliers-form/suppliers-form.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Supplier } from '../supplier.dto';
 
 @Component({
   selector: 'app-suppliers-form',
@@ -8,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SuppliersFormComponent implements OnInit {
 
-  constructor() { }
+  @Input() supplier!:Supplier;
+  supplierForm!: FormGroup;
+
+  constructor(private fb:FormBuilder) { }
 
   ngOnInit(): void {
+
+    this.supplierForm = this.fb.group({
+      id: [''],
+      companyName: [this.supplier.companyName, [Validators.required, Validators.minLength(3)]],
+      contactName: [this.supplier.contactName,[Validators.required, Validators.minLength(3)]],
+      contactTitle: [this.supplier.contactTitle],
+      address: this.fb.group(
+        {
+          city: [this.supplier.address.city],
+          country: [this.supplier.address.country],
+          phone: [this.supplier.address.phone],
+          postalCode: [this.supplier.address.postalCode],
+          region: [this.supplier.address.region],
+          street: [this.supplier.address.street]
+        }
+      )
+    })
+
+  }
+
+  onSubmit() {
+    console.log('onSubmit')
   }
 
 }
